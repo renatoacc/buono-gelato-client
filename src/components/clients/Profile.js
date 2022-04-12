2a3382af7860a10664ab6a76f54dbb25c6108ada
Renato Costa <renatoaccosta@gmail.com>
2022-04-12 08:56:58 +0100

    late updates

--- src/components/clients/Profile.js
+++ src/components/clients/Profile.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { API_BASE_URL } from "../../consts";
 import { AuthContext } from "../../context/AuthProvider";
@@ -7,6 +7,7 @@ import { AuthContext } from "../../context/AuthProvider";
 export default function Profile() {
   const navigate = useNavigate();
   const { user, addUserToContext } = useContext(AuthContext); // logout , removeUserFromContext
+  const [userInfo, setUserInfo] = useState({});
 
   async function getUser() {
     const { data } = await axios.get(API_BASE_URL + "/logged");
@@ -23,9 +24,49 @@ export default function Profile() {
     }
   }, []);
 
+  useEffect(() => {
+    try {
+      async function getUserInfo() {
+        const { data } = await axios.get(
+          API_BASE_URL + "/userInfo/" + user._id
+        );
+        setUserInfo(data);
+      }
+      getUserInfo();
+    } catch (error) {
+      console.log("Error get user information:", error);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Profile Page</h1>
+      <div>
+        <h3>Name: {userInfo.firstName + " " + userInfo.lastName}</h3>
+        <p>
+          <b>Address: </b>
+          {userInfo.address}
+        </p>
+        <p>
+          <b>City: </b>
+          {userInfo.city} <b>PostCode: </b>
+          {userInfo.postcode}
+        </p>
+        <p>
+          <b>Vat: </b>
+          {userInfo.vat}
+        </p>
+        <p>
+          <b>Phone: </b>
+          {userInfo.phone} <b>Email: </b>
+          {userInfo.email}
+        </p>
+      </div>
+      <div>
+        {/* {userInfo &&
+          userInfo.cart.map((elem) => {
+            <p>{elem.name}</p>;
+          })} */}
+      </div>
     </div>
   );
 }
