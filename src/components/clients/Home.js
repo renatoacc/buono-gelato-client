4005122aab627267a67697bfacbf35cb5e560928
Renato Costa <renatoaccosta@gmail.com>
2022-04-19 10:09:08 +0100

    guest

--- src/components/clients/Home.js
+++ src/components/clients/Home.js
@@ -12,6 +12,9 @@ export default function Home() {
         <Link className="link" to={"/signup"}>
           Signup
         </Link>
+        <Link className="link" to={"/profile"}>
+          Shop
+        </Link>
       </div>
     </div>
   );
